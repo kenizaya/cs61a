bc8941d09544bd65270228bfc4c3be8330d2700f
kenizaya <104677763+kenizaya@users.noreply.github.com>
2023-05-12 18:03:01 +0530

    Remove extra comments

--- projects/hw01/hw01.py
+++ projects/hw01/hw01.py
@@ -101,17 +101,14 @@ def with_if_function():
 
 
 def cond():
-    "*** YOUR CODE HERE ***"
     return False
 
 
 def true_func():
-    "*** YOUR CODE HERE ***"
     print(42)
 
 
 def false_func():
-    "*** YOUR CODE HERE ***"
     print(47)
 
 
